d8309be4ceb0c1901786bb9293b6c61e18d28e98
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-05-08 16:56:54 +0100

    Added functionality for Advanced Calculator

--- calculator.rb
+++ calculator.rb
@@ -14,9 +14,15 @@ def basic_calc(num1, num2, start_choice)
   end
 end
 
-# def adv_calc(num1, num2, start_choice)
-#   if start_choice ==
-# end
+def adv_calc(num1, num2, start_choice)
+  if start_choice == "s"
+    solution = Math.sqrt(num1)
+    puts solution
+  elsif start_choice == "p"
+    solution = num1 ** num2
+    puts solution
+  end
+end
 
 calc_prompt = "What calculator do you want to use? (B)asic | (A)dvanced"
 puts "#{calc_prompt}"
@@ -38,4 +44,19 @@ if calc_choice == "b"
 elsif calc_choice == "a"
   advance_prompt = "What would you like to do? (S)quareroot | (P)ower"
   puts "#{advance_prompt}"
+  start_choice = gets.chomp
+
+  if start_choice == "s"
+    puts "First number..."
+    first_num = gets.to_i
+    adv_calc(first_num,start_choice)
+  elsif start_choice== "p"
+    puts "First number..."
+    first_num = gets.to_i
+
+    puts "Second number..."
+    second_num = gets.to_i
+
+    adv_calc(first_num, second_num, start_choice)
+  end
 end
