7877fa1e01de9600d8142471f2a788b5cfab8f83
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-05-09 09:15:32 +0100

    Calculator nowasks if you want to conitue using it after use.

--- calculator.rb
+++ calculator.rb
@@ -24,39 +24,51 @@ def adv_calc(num1, num2, start_choice)
   end
 end
 
-calc_prompt = "What calculator do you want to use? (B)asic | (A)dvanced"
-puts "#{calc_prompt}"
-calc_choice = gets.chomp
+def calculator
+  calc_prompt = "What calculator do you want to use? (B)asic | (A)dvanced | (Q)uit"
+  puts "#{calc_prompt}"
+  calc_choice = gets.chomp
 
 
-if calc_choice == "b"
-  start_prompt = "What do you want to do? (A)dd | (S)ubtract | (M)ultiply | (D)ivide"
-  puts "#{start_prompt}"
-  start_choice = gets.chomp
+  if calc_choice == "b"
+    start_prompt = "What do you want to do? (A)dd | (S)ubtract | (M)ultiply | (D)ivide"
+    puts "#{start_prompt}"
+    start_choice = gets.chomp
 
-  puts "First number..."
-  first_num = gets.to_i
-
-  puts "Second number..."
-  second_num = gets.to_i
-
-  basic_calc(first_num, second_num, start_choice)
-elsif calc_choice == "a"
-  advance_prompt = "What would you like to do? (S)quareroot | (P)ower"
-  puts "#{advance_prompt}"
-  start_choice = gets.chomp
-
-  if start_choice == "s"
-    puts "First number..."
-    first_num = gets.to_i
-    adv_calc(first_num,start_choice)
-  elsif start_choice== "p"
     puts "First number..."
     first_num = gets.to_i
 
     puts "Second number..."
     second_num = gets.to_i
 
-    adv_calc(first_num, second_num, start_choice)
+    basic_calc(first_num, second_num, start_choice)
+  elsif calc_choice == "a"
+    advance_prompt = "What would you like to do? (S)quareroot | (P)ower"
+    puts "#{advance_prompt}"
+    start_choice = gets.chomp
+
+    if start_choice == "s"
+      puts "First number..."
+      first_num = gets.to_i
+      adv_calc(first_num,start_choice)
+    elsif start_choice== "p"
+      puts "First number..."
+      first_num = gets.to_i
+
+      puts "Second number..."
+      second_num = gets.to_i
+
+      adv_calc(first_num, second_num, start_choice)
+    end
   end
+end
+
+loop do
+  calculator
+
+  continue_prompt = "Continue using the calculator? (Y)es | (N)o"
+  puts "#{continue_prompt}"
+  continue_choice = gets.chomp
+
+  break if continue_choice == "n" || calc_choice == "q"
 end
